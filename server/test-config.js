e1919614c88d4221184f5f4d8defe98683d97acb
gitgrimbo <gitgrimbo@gmail.com>
2016-10-22 10:11:43 +0100

    Prefer non-local host names

--- server/test-config.js
+++ server/test-config.js
@@ -1,6 +1,20 @@
+// E.g. for Sauce Labs, but also works for local testing.
+// Put these entries in your hosts file.
+
+module.exports = {
+  http1: "resource-prefetch-tests1",
+  http2: "resource-prefetch-tests2",
+  https1: "resource-prefetch-tests1",
+  https2: "resource-prefetch-tests2"
+};
+
+// Local testing only.  Prefer the above if possible.
+
+/*
 module.exports = {
   http1: "localhost",
   http2: "127.0.0.1",
   https1: "localhost",
   https2: "127.0.0.1"
 };
+*/
