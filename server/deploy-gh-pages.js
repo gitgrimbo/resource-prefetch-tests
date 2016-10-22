b506c83272d00c30c971d4012ce4a2958485fd0c
gitgrimbo <gitgrimbo@gmail.com>
2016-10-22 10:12:52 +0100

    Improve gh-pages deployment

--- server/deploy-gh-pages.js
+++ server/deploy-gh-pages.js
@@ -1,10 +1,50 @@
 /* eslint-env node, shelljs */
+const fs = require("fs");
 const path = require("path");
 const resultsPage = require("./results-page");
 require("shelljs/global");
+const pkg = require("../package.json");
 
-const ghTemp = "./temp/gh-pages";
-const repo = require("../../package.json").repository.url;
+function readJson(file) {
+  const json = fs.readFileSync(file);
+  return JSON.parse(json);
+}
+
+function listFiles(dir) {
+  return fs.readdirSync(dir).map(f => path.resolve(dir, f));
+}
+
+function generateResultHtmls(dir, resultFiles) {
+  resultFiles.forEach(resultFile => {
+    console.log(resultFile);
+    const html = resultsPage(readJson(resultFile), "./style.css");
+    const htmlFile = path.join(dir, path.parse(resultFile).name + ".html");
+    console.log(htmlFile);
+    fs.writeFileSync(htmlFile, html);
+  });
+}
+
+function generateIndexHtml(resultFiles) {
+  const pre = "<!doctype html>\n";
+  const post = "";
+
+  const link = f => {
+    const name = path.parse(f).name;
+    return `<a href="${name}.html">${name}</a>`;
+  };
+
+  return pre + resultFiles.map(f => {
+    return `<li>${link(f)}</li>`;
+  }).join("\n") + post;
+}
+
+const resultsDir = path.resolve("results");
+console.log("resultsDir=" + resultsDir);
+
+const ghTemp = path.resolve("./temp/gh-pages");
+console.log("ghTemp=" + ghTemp);
+
+const repo = pkg.repository.url;
 
 set("+v");
 
@@ -14,16 +54,20 @@ if (!which("git")) {
 }
 
 rm("-rf", ghTemp);
-mkdir("-p", ghTemp);
 
+// git clone will also create the ghTemp folder
 exec("git clone " + repo + " --branch gh-pages --single-branch " + ghTemp);
 
-const html = resultsPage({}, "../styles.css");
-console.log(html);
-/*
+const resultFiles = listFiles(resultsDir);
+generateResultHtmls(ghTemp, resultFiles);
+
+cp("./samples/style.css", ghTemp);
+
+const indexHtml = generateIndexHtml(resultFiles);
+fs.writeFileSync(path.join(ghTemp, "index.html"), indexHtml);
+
 cd(ghTemp);
 
 exec("git add .");
 exec("git commit --amend --no-edit");
 exec("git push -f origin gh-pages");
-*/
