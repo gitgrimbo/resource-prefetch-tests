44161dc988348ea36dd791c538c738e2d96c9dc3
gitgrimbo <gitgrimbo@gmail.com>
2016-10-26 23:42:46 +0100

    Add console polyfill for IE9, etc

--- server/vendor-script-mappings.js
+++ server/vendor-script-mappings.js
@@ -1,5 +1,6 @@
 module.exports = [
   ["/bluebird.js", "/bluebird/js/browser/bluebird.js"],
+  ["/console-polyfill.js", "/console-polyfill/index.js"],
   ["/jquery.js", "/jquery/dist/jquery.js"],
   ["/mustache.js", "/mustache/mustache.js"],
   ["/require.js", "/requirejs/require.js"]
