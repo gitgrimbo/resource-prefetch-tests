8e3d3509d4c00fb7e1988f557edd06baec1d72c4
gitgrimbo <gitgrimbo@gmail.com>
2016-11-02 16:49:07 +0000

    Move getUrlParams into url module

--- public/url.js
+++ public/url.js
@@ -14,8 +14,36 @@ define([], function() {
     return url + (parts.length > 1 ? "&" : "?") + qs;
   }
 
+  // Based on
+  // http://stackoverflow.com/a/2880929/319878
+
+  window.urlParams = null;
+
+  function getUrlParams(query) {
+    if (typeof query === "undefined") {
+      query = window.location.search.substring(1);
+    }
+
+    var match,
+      pl = /\+/g,  // Regex for replacing addition symbol with a space
+      search = /([^&=]+)=?([^&]*)/g,
+      decode = function(s) {
+        return decodeURIComponent(s.replace(pl, " "));
+      };
+
+    var urlParams = {};
+    while (match = search.exec(query))
+      urlParams[decode(match[1])] = decode(match[2]);
+    return urlParams;
+  }
+
+  (window.onpopstate = function() {
+    window.urlParams = getUrlParams();
+  })();
+
   return {
     toQueryString: toQueryString,
-    addParams: addParams
+    addParams: addParams,
+    getUrlParams: getUrlParams
   };
 });
