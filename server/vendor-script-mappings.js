7da5842bc3837f177189710eb0c1cbc3f41d2084
gitgrimbo <gitgrimbo@gmail.com>
2016-11-02 16:47:35 +0000

    Use minified vendor scripts

--- server/vendor-script-mappings.js
+++ server/vendor-script-mappings.js
@@ -1,8 +1,8 @@
 module.exports = [
-  ["/bluebird.js", "/bluebird/js/browser/bluebird.js"],
+  ["/bluebird.js", "/bluebird/js/browser/bluebird.min.js"],
   ["/console-polyfill.js", "/console-polyfill/index.js"],
   ["/es6-object-assign.js", "/es6-object-assign/dist/object-assign.min.js"],
-  ["/jquery.js", "/jquery/dist/jquery.js"],
-  ["/mustache.js", "/mustache/mustache.js"],
+  ["/jquery.js", "/jquery/dist/jquery.min.js"],
+  ["/mustache.js", "/mustache/mustache.min.js"],
   ["/require.js", "/requirejs/require.js"]
 ];
