17645fb31e18515aee510be48c6d42050471a164
gitgrimbo <gitgrimbo@gmail.com>
2016-11-03 21:48:41 +0000

    Object.assign now covered by babel-polyfill

--- server/vendor-script-mappings.js
+++ server/vendor-script-mappings.js
@@ -2,7 +2,6 @@ module.exports = [
   ["/babel-polyfill.js", "/babel-polyfill/dist/polyfill.min.js"],
   ["/bluebird.js", "/bluebird/js/browser/bluebird.min.js"],
   ["/console-polyfill.js", "/console-polyfill/index.js"],
-  ["/es6-object-assign.js", "/es6-object-assign/dist/object-assign.min.js"],
   ["/jquery.js", "/jquery/dist/jquery.min.js"],
   ["/mustache.js", "/mustache/mustache.min.js"]
 ];
